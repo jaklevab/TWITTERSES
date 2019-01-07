6effdc040d22cd9e5e9aa19566fbeafb2bf49db3
jaklevab <jacob.levy-abitbol@ens-lyon.fr>
2019-01-07 13:26:17 +0100

    Typo fixing

--- python_scripts/certified/helpers_ses_enrichment.py
+++ python_scripts/certified/helpers_ses_enrichment.py
@@ -54,7 +54,7 @@ def generate_insee_ses_data(f_data="/warehouse/COMPLEXNET/jlevyabi/TWITTERSES/ge
     new_geo=[]
     presentable_insee_df=pd.read_csv(f_data,sep=";")
     presentable_insee_df.rename({"Unnamed: 0":"position"},inplace=True)
-    new_geo = [Polygon([tuple(map(float, m.split())) for m in pat.findall(geo)]
+    new_geo = [Polygon([tuple(map(float, m.split())) for m in pat.findall(geo)])
                        if pat.findall(geo) else None for geo in tqdm(df_insee.geometry)]
     presentable_insee_df=presentable_insee_df.convert_objects(convert_numeric=True)
     presentable_insee_df['geometry']=new_geo
